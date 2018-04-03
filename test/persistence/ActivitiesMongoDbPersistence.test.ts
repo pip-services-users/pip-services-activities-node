148418eea62acbb7b82eb4bbec1ce2dce66dbf2e
Dmitriy Krayniy <krdima92@gmail.com>
2018-04-03 14:50:36 +0300

    changed MongoDbPersistence test

--- test/persistence/ActivitiesMongoDbPersistence.test.ts
+++ test/persistence/ActivitiesMongoDbPersistence.test.ts
@@ -1,4 +1,6 @@
-import { YamlConfigReader } from 'pip-services-commons-node';
+let process = require('process');
+
+import { ConfigParams } from 'pip-services-commons-node';
 
 import { ActivitiesMongoDbPersistence } from '../../src/persistence/ActivitiesMongoDbPersistence';
 import { ActivitiesPersistenceFixture } from './ActivitiesPersistenceFixture';
@@ -8,8 +10,19 @@ suite('ActivitiesMongoDbPersistence', ()=> {
     let fixture: ActivitiesPersistenceFixture;
 
     setup((done) => {
-        let config = YamlConfigReader.readConfig(null, './config/test_connections.yml', null);
-        let dbConfig = config.getSection('mongodb');
+        var MONGO_DB = process.env["MONGO_DB"] || "test";
+        var MONGO_COLLECTION = process.env["MONGO_COLLECTION"] || "activities";
+        var MONGO_SERVICE_HOST = process.env["MONGO_SERVICE_HOST"] || "localhost";
+        var MONGO_SERVICE_PORT = process.env["MONGO_SERVICE_PORT"] || "27017";
+        var MONGO_SERVICE_URI = process.env["MONGO_SERVICE_URI"];
+
+        var dbConfig = ConfigParams.fromTuples(
+            "collection", MONGO_COLLECTION,
+            "connection.database", MONGO_DB,
+            "connection.host", MONGO_SERVICE_HOST,
+            "connection.port", MONGO_SERVICE_PORT,
+            "connection.uri", MONGO_SERVICE_URI
+        );
 
         persistence = new ActivitiesMongoDbPersistence();
         persistence.configure(dbConfig);
@@ -17,9 +30,13 @@ suite('ActivitiesMongoDbPersistence', ()=> {
         fixture = new ActivitiesPersistenceFixture(persistence);
 
         persistence.open(null, (err: any) => {
-            persistence.clear(null, (err) => {
+            if (err == null) {
+                persistence.clear(null, (err) => {
+                    done(err);
+                });
+            } else {
                 done(err);
-            });
+            }
         });
     });
     

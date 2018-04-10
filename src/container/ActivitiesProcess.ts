8178cfe26cf0a42ba13768aae46bd358ccf2254a
Dmitriy Krayniy <krdima92@gmail.com>
2018-04-10 15:37:44 +0300

    added pip-services-oss

--- src/container/ActivitiesProcess.ts
+++ src/container/ActivitiesProcess.ts
@@ -1,5 +1,6 @@
 import { IReferences } from 'pip-services-commons-node';
 import { ProcessContainer } from 'pip-services-container-node';
+import { DefaultOssFactory } from 'pip-services-oss-node';
 
 import { ActivitiesServiceFactory } from '../build/ActivitiesServiceFactory';
 
@@ -8,5 +9,6 @@ export class ActivitiesProcess extends ProcessContainer {
     public constructor() {
         super("activities", "Party activities microservice");
         this._factories.add(new ActivitiesServiceFactory);
+        this._factories.add(new DefaultOssFactory);
     }
 }

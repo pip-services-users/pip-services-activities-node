f50ad18a2bde436606bc060e25b3a3c6bea4cd55
pipdeveloper <pipdevs@gmail.com>
2018-05-18 16:20:36 +0000

    project build by Travis CI [skip ci]

--- obj/src/services/version1/ActivitiesHttpServiceV1.js
+++ obj/src/services/version1/ActivitiesHttpServiceV1.js
@@ -4,7 +4,7 @@ const pip_services_commons_node_1 = require("pip-services-commons-node");
 const pip_services_net_node_1 = require("pip-services-net-node");
 class ActivitiesHttpServiceV1 extends pip_services_net_node_1.CommandableHttpService {
     constructor() {
-        super('activities');
+        super('v1/activities');
         this._dependencyResolver.put('controller', new pip_services_commons_node_1.Descriptor('pip-services-activities', 'controller', 'default', '*', '1.0'));
     }
 }

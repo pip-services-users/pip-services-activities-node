9211905b2e59d900626be148f3e6adc0c567a9ff
pipdeveloper <pipdevs@gmail.com>
2019-06-03 18:23:55 +0000

    project build by Travis CI [skip ci]

--- obj/src/persistence/index.d.ts
+++
@@ -1,5 +0,0 @@
-export { IActivitiesPersistence } from './IActivitiesPersistence';
-export { ActivitiesMemoryPersistence } from './ActivitiesMemoryPersistence';
-export { ActivitiesFilePersistence } from './ActivitiesFilePersistence';
-export { ActivitiesMongoDbPersistence } from './ActivitiesMongoDbPersistence';
-export { ActivitiesCouchbasePersistence } from './ActivitiesCouchbasePersistence';

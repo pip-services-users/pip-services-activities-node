6e967296113575a07e2cfa70c9b762cfa7369071
pipdeveloper <pipdevs@gmail.com>
2019-07-09 05:34:27 +0000

    project build by Travis CI [skip ci]

--- obj/src/services/version1/ActivitiesCommandableGrpcServiceV1.d.ts
+++
@@ -1,4 +0,0 @@
-import { CommandableGrpcService } from 'pip-services3-grpc-node';
-export declare class ActivitiesCommandableGrpcServiceV1 extends CommandableGrpcService {
-    constructor();
-}
--- obj/src/services/version1/ActivitiesHttpServiceV1.d.ts
+++
@@ -1,4 +0,0 @@
-import { CommandableHttpService } from 'pip-services3-rpc-node';
-export declare class ActivitiesHttpServiceV1 extends CommandableHttpService {
-    constructor();
-}

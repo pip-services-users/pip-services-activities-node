a7735a80ab2ab309613a43cf259d38d8d07aa575
pipdeveloper <pipdevs@gmail.com>
2019-07-13 02:04:44 +0000

    project build by Travis CI [skip ci]

--- obj/src/services/version1/ActivitiesGrpcServiceV1.d.ts
+++ obj/src/services/version1/ActivitiesGrpcServiceV1.d.ts
@@ -4,9 +4,9 @@ export declare class ActivitiesGrpcServiceV1 extends GrpcService {
     private _controller;
     constructor();
     setReferences(references: IReferences): void;
-    private getPartyActivities;
-    private logPartyActivity;
-    private batchPartyActivities;
-    private deletePartyActivities;
+    private getPartyActivities(call, callback);
+    private logPartyActivity(call, callback);
+    private batchPartyActivities(call, callback);
+    private deletePartyActivities(call, callback);
     register(): void;
 }
